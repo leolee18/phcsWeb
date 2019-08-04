664e47b88386d0cd106fe6c6112e2302e58693bd
leolee18 <wwwliyuxi@163.com>
2019-08-04 15:02:57 +0800

    图片生成改版本

--- src/views/Composite.vue
+++ src/views/Composite.vue
@@ -3,12 +3,16 @@
 	<div class="com-ts">长按下方图片，『保存图片』或『发送给朋友』</div>
 	<img ref="myImg" src="" class="com-bc-img"/>
 	<div class="com-can">
-		<canvas ref="myChartI" width="750px" height="1230px"></canvas>
+		<canvas ref="myChartI" width="630px" height="1036px"></canvas>
 	</div>
 	<img :src="musimg" class="com-can-img"  @load="ULoaded" ref="mUimg" />
 	<img :src="mtsimg" class="com-can-img" @load="TLoaded" ref="mTimg" />
 	<img :src="mJGImg" class="com-can-img" @load="CSLoaded" ref="mCSimg" />
-	<img src="../assets/erm.jpg" class="com-can-img" @load="ERLoaded" ref="mERimg" />
+	<img src="../assets/hct/user.png" class="com-can-img" @load="HctUserL" ref="mHctU" />
+	<img src="../assets/hct/zj_bs.png" class="com-can-img" @load="HctBsL" ref="mHctBs" />
+	<img src="../assets/hct/zj_kx.png" class="com-can-img" @load="HctKxL" ref="mHctKx" />
+	<img src="../assets/hct/fgx.png" class="com-can-img" @load="HctFxL" ref="mHctFx" />
+	<img src="../assets/hct/ewm.png" class="com-can-img" @load="ERLoaded" ref="mERimg" />
   </div>
 </template>
 
@@ -49,14 +53,13 @@ export default {
 		that.mTimg = this.$refs.mTimg;
 		that.mCSImg = this.$refs.mCSimg;
 		that.mERImg = this.$refs.mERimg;
+		
+		that.mHctU = this.$refs.mHctU;
+		that.mHctBs = this.$refs.mHctBs;
+		that.mHctKx = this.$refs.mHctKx;
+		that.mHctFx = this.$refs.mHctFx;
+		
 		that.ctx = that.$refs.myChartI.getContext('2d');
-		that.ctx.fillStyle="#ffffff";
-		that.ctx.fillRect(0,0,750,1230);
-		that.ctx.font="30px Arial";
-		that.ctx.fillStyle="#8b9eab";
-		that.ctx.fillText("我的测试结果",500,110);
-		that.ctx.font="28px Arial";
-		that.ctx.fillText("长按图片，测测你的平衡状态",50,1150);
 		
 		if(this.mResObj && this.mResObj.tjImg){
 			this.mtsimg = 'http://51coach.com/wwlyweb/'+this.mResObj.tjImg;
@@ -76,49 +79,62 @@ export default {
 	},
 	methods: {
 		ULoaded(e){
-			let circle = {
-				x: 50+30,
-				y: 50+50,
-				r: 50
-			}
-			this.ctx.save();
-			this.ctx.beginPath();
-			this.ctx.arc(circle.x, circle.y, circle.r, 0, Math.PI * 2, false);
-			this.ctx.clip();
-			this.ctx.drawImage(this.mUimg,30,50,100,100);
-			this.ctx.restore();
+			this.ctx.fillStyle="#ffffff";
+			this.ctx.fillRect(37,505,72,72);
+			this.ctx.drawImage(this.mUimg,37,505,72,72);
 			this.updateImg();
 		},
 		TLoaded(e){
-			this.ctx.drawImage(this.mTimg,10,160);
+			this.ctx.drawImage(this.mTimg,28,28);
 			this.updateImg();
 		},
 		CSLoaded(e){
-			this.ctx.drawImage(this.mCSImg,10,720,330,330);
+			this.ctx.drawImage(this.mCSImg,360,500,240,240);
 			this.updateImg();
 		},
 		ERLoaded(e){
-			this.ctx.drawImage(this.mERImg,530,1050,150,150);
+			this.ctx.drawImage(this.mERImg,430,780);
+			this.updateImg();
+		},
+		HctUserL(e){
+			this.ctx.drawImage(this.mHctU,32,500);
+			this.updateImg();
+		},
+		HctBsL(e){
+			this.ctx.drawImage(this.mHctKx,32,700);
+			this.updateImg();
+		},
+		HctKxL(e){
+			this.ctx.drawImage(this.mHctBs,32,855);
+			this.updateImg();
+		},
+		HctFxL(e){
+			this.ctx.drawImage(this.mHctFx,32,998);
 			this.updateImg();
 		},
 		canText(){
 			let that = this;
 			
+			that.ctx.fillStyle = that.bgColor(that.mResObj.tjImg);
+			that.ctx.fillRect(0,0,630,1036);
+			that.ctx.font="28px Arial";
 			that.ctx.fillStyle="#000000";
-			that.ctx.font="30px Arial";
-			let mNameS = '匿名';
-			if(this.mResObj && this.mResObj.user && this.mResObj.user.nickname && this.mResObj.user.nickname != ''){
-				mNameS = this.mResObj.user.nickname;
-			}
-			that.ctx.fillText(mNameS,150,110);
+			that.ctx.fillText("扫 描 二 维 码",430,990);
+			that.ctx.font="22px Arial";
+			that.ctx.fillText("测测你的平衡状态",430,1020);
 			
-			that.ctx.font="26px Arial";
-			that.ctx.fillStyle="#9ca1ac";
+			that.ctx.fillStyle="#000000";
+			that.ctx.font="33px Arial";
+			that.ctx.fillText('我的',167,550);
+			that.ctx.fillText('平衡状态',167,600);
+			
+			that.ctx.font="22px Arial";
+			that.ctx.fillStyle="#1f313e";
 			if(that.mdaMS.length >0){
-				that.drawText(that.ctx,that.mdaMS[0],380,750,300,34);
+				that.drawText(that.ctx,that.mdaMS[0],95,715,300,32);
 			}
 			if(that.mdaMS.length >1){
-				that.drawText(that.ctx,that.mdaMS[1],380,920,300,34);
+				that.drawText(that.ctx,that.mdaMS[1],88,870,300,32);
 			}
 		},
 		drawText(ctx, text, x, y, maxWidth, lineHeight) {
@@ -138,6 +154,36 @@ export default {
 				 }
 			}
 			ctx.fillText(line, x, y);
+		},
+		bgColor(mStr){
+			let mColor = '#f4b382';
+			switch (mStr){
+				case 'imgs/tjt/tjt_ah.png':
+					mColor = '#f4b382';
+					break;
+				case 'imgs/tjt/tjt_cf.png':
+					mColor = '#dbab44';
+					break;
+				case 'imgs/tjt/tjt_jt.png':
+					mColor = '#eb9690';
+					break;
+				case 'imgs/tjt/tjt_jk.png':
+					mColor = '#9fa268';
+					break;
+				case 'imgs/tjt/tjt_sj.png':
+					mColor = '#7eb569';
+					break;
+				case 'imgs/tjt/tjt_sy.png':
+					mColor = '#8899dc';
+					break;
+				case 'imgs/tjt/tjt_wh.png':
+					mColor = '#e897c0';
+					break;
+				case 'imgs/tjt/tjt_xx.png':
+					mColor = '#9c93cd';
+					break;							
+			}
+			return mColor;
 		},
 		updateImg(){
 			let myImg = this.$refs.myImg;
@@ -181,5 +227,6 @@ export default {
 		display: block;
 		width: 630px;
 		margin: 20px auto;
+		box-shadow: 0 2px 5px #8A9DAA;
 	}
 </style>
